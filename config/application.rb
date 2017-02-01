1d71da60cbdbd7f8678bb6aadd42ed1e96caf50f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-02-02 02:07:02 +0300

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Trickster
+  class Application < Rails::Application
+    config.time_zone = 'Moscow'
+
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ru
+
+    %w(app/services lib).each do |path|
+      config.autoload_paths << config.root.join(path).to_s
+    end
+  end
+end
